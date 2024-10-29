850be6c5055cd1c408914c9e2cb231b6f64dd96f
Kieran Brown <KBrown2598@student.pps.net>
2024-10-29 11:39:35 -0700

    Add mecanum wheel support

---
+++ src/main/java/com/mcdanielpps/mechframework/motion/MecanumWheelController.java
@@ -0,0 +1,51 @@
+package com.mcdanielpps.mechframework.motion;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumWheelController {
+    public DcMotor FL = null;
+    public DcMotor FR = null;
+    public DcMotor RL = null;
+    public DcMotor RR = null;
+
+    public boolean InvertFL = false;
+    public boolean InvertFR = false;
+    public boolean InvertRL = false;
+    public boolean InvertRR = false;
+
+    // Helper function to convert the boolean invert input to a coefficient that can be used for the math
+    private double CastInvert(boolean invert) {
+        return invert ? -1.0 : 1.0;
+    }
+
+    public void UpdateWheels(double theta, double power, double turn, double speedCoefficient) {
+        // Copied from https://www.youtube.com/watch?v=gnSW2QpkGXQ
+
+        double sin = Math.sin(theta - Math.PI / 4.0);
+        double cos = Math.cos(theta - Math.PI / 4.0);
+        double max = Math.max(Math.abs(sin), Math.abs(cos));
+
+        double leftFront = power * cos / max + CastInvert(InvertFL) * turn;
+        double rightFront = power * sin / max - CastInvert(InvertFR) * turn;
+        double leftRear = power * sin / max + CastInvert(InvertRL) * turn;
+        double rightRear = power * cos / max - CastInvert(InvertRR) * turn;
+
+        if ((power + Math.abs(turn)) > 1.0) {
+            leftFront /= power + turn;
+            rightFront /= power + turn;
+            leftRear /= power + turn;
+            rightRear /= power + turn;
+        }
+
+        FL.setPower(leftFront * speedCoefficient);
+        FR.setPower(rightFront * speedCoefficient);
+        RL.setPower(leftRear * speedCoefficient);
+        RR.setPower(rightRear * speedCoefficient);
+    }
+
+    public void UpdateWheelsGamepad(double x, double y, double turn, double speedCoefficient) {
+        double theta = Math.atan2(y, x);
+        double power = Math.hypot(x, y);
+        UpdateWheels(theta, power, turn, speedCoefficient);
+    }
+}
