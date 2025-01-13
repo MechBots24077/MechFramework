23da0de90385ac5e2f2875728db08f0a92adca67
Kieran Brown <KBrown2598@student.pps.net>
2025-01-13 15:03:27 -0800

    Add FTC Dashboard

--- src/main/java/com/mcdanielpps/mechframework/motion/MotorController.java
+++ src/main/java/com/mcdanielpps/mechframework/motion/MotorController.java
@@ -1,5 +1,6 @@
 package com.mcdanielpps.mechframework.motion;
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.mcdanielpps.mechframework.util.PIDController;
 import com.mcdanielpps.mechframework.util.Time;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,15 +12,12 @@ public class MotorController {
     public int Goal = 0;
     private int m_CurrentPosition = 0;
 
-    public final PIDController m_PID;
+    private final PIDController m_PID;
     private long m_LastMeasurement = 0;
 
     public MotorController(DcMotor motor, double kp, double ki, double kd) {
         Motor = motor;
         Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        double ku = 5.0;
-        double pu = 0.2;
 
         // 2.5, 0.1, 0.01
         m_PID = new PIDController(kp, ki, kd, 0.02, -100.0, 100.0, 0.01);
@@ -30,7 +28,7 @@ public class MotorController {
         Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public double Update() {
+    public double Update(TelemetryPacket packet, String name) {
         long currentTime = Time.TimeGetter.currentTimeMillis();
         if ((currentTime - m_LastMeasurement) < 10) { return 0.0; }
         m_LastMeasurement = currentTime;
@@ -40,7 +38,17 @@ public class MotorController {
         double output = m_PID.Update((double)Goal, (double)m_CurrentPosition);
         Motor.setPower(output / 100.0);
 
+        packet.put(name + ", Position", m_CurrentPosition);
+        packet.put(name + ", Goal", Goal);
+        packet.put(name + ", Power", output / 100.0);
+
         return output;
+    }
+
+    public void UpdatePID(double kp, double ki, double kd) {
+        m_PID.Kp = kp;
+        m_PID.Ki = ki;
+        m_PID.Kd = kd;
     }
 
     public int GetCurrentPos() { return m_CurrentPosition; }
